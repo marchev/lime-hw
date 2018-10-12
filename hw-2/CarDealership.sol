a278e77ff253f8523911f79da3839547da611bce
marchev <n1tmare@gmail.com>
2018-10-12 09:38:14 +0300

    Oops

--- hw-2/CarDealership.sol
+++ hw-2/CarDealership.sol
@@ -1,11 +1,54 @@
-" ============================================================================
-" Netrw Directory Listing                                        (netrw v156)
-"   /home/marchev/lime-academy
-"   Sorted by      name
-"   Sort sequence: [\/]$,\<core\%(\.\d\+\)\=\>,\.h$,\.c$,\.cpp$,\~\=\*$,*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$
-"   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:special
-" ==============================================================================
-../
-./
-billboard.sol
-notes.txt
+pragma solidity ^0.4.25;
+
+contract CarDealership {
+    
+    address public dealershipOwner;
+    
+    struct Car {
+        string make;
+        string model;
+    }
+
+    mapping(uint256 => Car) cars;
+    
+    constructor(address _dealershipOwner) {
+        dealershipOwner = _dealershipOwner;
+    }
+    
+    function addCar(string make, string model) {
+        Car memory newCar = Car(make, model);
+        uint256 newCarKey = uint256(
+            keccak256(
+                abi.encodePacked(make, model)
+            )
+        );
+        cars[newCarKey] = newCar;
+        
+    }
+    
+    //     modifier onlyPositive (uint256 _newPrice) {
+    //     require(_newPrice > 0, "The given price must be greater than zero.");
+    //     _;
+    // }
+    
+    // function buy(string _slogan) public payable {
+    //     require(msg.value >= price, "The given value is too low");
+        
+    //     slogan = _slogan;
+    //     billboardOwner = msg.sender;
+    //     historyOwners.push(msg.sender);
+    //     moneySpent[msg.sender] += msg.value;
+        
+    //     emit LogBillboardBought(_slogan, msg.sender);
+    // }
+    
+    // function setNewPrice(uint256 _newPrice) public onlyPositive(_newPrice) onlyOwner() {
+    //     price = _newPrice;
+    // }
+    
+    // function withdrawFunds() public returns(bool) {
+    //     owner.transfer(address(this).balance);
+    
+    //     emit LogWithdrawal(address(this).balance);
+    // }
+}
